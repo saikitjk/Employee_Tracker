e834bba5aa9d6fc310cc1d0a7f2de0e1430dfaa6
JK <saikitjk@gmail.com>
2020-09-03 17:54:51 -0700

    created files and connection to localhose

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const util = require("util");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "",
+});
+
+connection.connect();
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
