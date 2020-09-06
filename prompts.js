cc8c37285c479e5257186cccf806ad6e092bc1bb
JK <saikitjk@gmail.com>
2020-09-06 12:05:32 -0700

    restructured the file system and have the first prompt and view function made

--- prompts.js
+++ prompts.js
@@ -1,48 +1,48 @@
-const inquirer = require("inquirer");
-const db = require("./db/queries");
-require("console.table");
+// const inquirer = require("inquirer");
+// const db = require("./db/queries");
+// require("console.table");
 
-// async function mainPrompt() {
-//   console.log("\n Welcome to Employee Tracker \n");
-//   inquirer
-//     .prompt({
-//       name: "action",
-//       type: "list",
-//       message: "What do you want to do?",
-//       choices: [
-//         "View all employees",
-//         "View all employees by role",
-//         "View all employees by department",
-//         "View all employees by manager",
-//         "Add employee",
-//         "Add role",
-//         "Add department",
-//         "Update employee role",
-//         "Update employee manager",
-//         "Delete employee",
-//         "Delete role",
-//         "Delete department",
-//         "View department budgets",
-//       ],
-//     })
-//     .then((answer) => {
-//       switch (answer.action) {
-//         case "View all employees":
-//           console.log("second checkpoint");
-//           return viewAllEmp();
+module.exports = {
+  mainPrompt: [
+    {
+      name: "action",
+      type: "list",
+      message: "What do you want to do?",
+      choices: [
+        "View all employees",
+        "View all employees by role",
+        "View all employees by department",
+        "View all employees by manager",
+        "Add employee",
+        "Add role",
+        "Add department",
+        "Update employee role",
+        "Update employee manager",
+        "Delete employee",
+        "Delete role",
+        "Delete department",
+        "View department budgets",
+      ],
+    },
+  ],
+}; //module.export ends here
 
-//         //return viewAllEmp();
+// .then((answer) => {
+//   switch (answer.action) {
+//     case "View all employees":
+//       console.log("second checkpoint");
+//       return viewAllEmp();
+
+//return viewAllEmp();
 //       }
 //     });
 // }
 
-// module.exports = mainPrompt();
+// module.exports = {
+//   viewAllEmp: function () {
+//     const allEmp = db.viewDbAllEmp();
 
-module.exports = {
-  viewAllEmp: function () {
-    const allEmp = db.viewDbAllEmp();
-
-    //console.log("\n");
-    return allEmp;
-  },
-};
+//     //console.log("\n");
+//     return allEmp;
+//   },
+// };
--- server.js
+++ server.js
@@ -4,46 +4,60 @@ const prompts = require("./prompts");
 const db = require("./db/queries");
 require("console.table");
 
-// async function viewAllEmp() {
-//   const allEmp = await db.viewDbAllEmp();
+async function main() {
+  const { action } = await inquirer.prompt(prompts.mainPrompt);
 
-//   console.log("\n");
-//   console.table(allEmp);
-// }
-// //viewAllEmp();
-async function mainPrompt() {
-  console.log("\n Welcome to Employee Tracker \n");
-  inquirer
-    .prompt({
-      name: "action",
-      type: "list",
-      message: "What do you want to do?",
-      choices: [
-        "View all employees",
-        "View all employees by role",
-        "View all employees by department",
-        "View all employees by manager",
-        "Add employee",
-        "Add role",
-        "Add department",
-        "Update employee role",
-        "Update employee manager",
-        "Delete employee",
-        "Delete role",
-        "Delete department",
-        "View department budgets",
-      ],
-    })
-    .then((answer) => {
-      switch (answer.action) {
-        case "View all employees":
-          console.log("second checkpoint");
-          console.log("\n");
-          console.table(prompts.viewAllEmp());
-
-        //return viewAllEmp();
-      }
-    });
+  switch (action) {
+    case "View all employees":
+      console.log("second checkpoint");
+      viewAllEmp();
+  }
 }
 
-mainPrompt();
+async function viewAllEmp() {
+  const allEmp = await db.viewDbAllEmp();
+
+  console.log("\n");
+  console.table(allEmp);
+
+  main();
+}
+
+main();
+
+// async function mainPrompt() {
+//   console.log("\n Welcome to Employee Tracker \n");
+//   inquirer
+//     .prompt({
+//       name: "action",
+//       type: "list",
+//       message: "What do you want to do?",
+//       choices: [
+//         "View all employees",
+//         "View all employees by role",
+//         "View all employees by department",
+//         "View all employees by manager",
+//         "Add employee",
+//         "Add role",
+//         "Add department",
+//         "Update employee role",
+//         "Update employee manager",
+//         "Delete employee",
+//         "Delete role",
+//         "Delete department",
+//         "View department budgets",
+//       ],
+//     })
+//     .then((answer) => {
+//       switch (answer.action) {
+//         case "View all employees":
+//           console.log("second checkpoint");
+//           console.log("\n");
+//           console.table(prompts.viewAllEmp());
+
+//         //return viewAllEmp();
+//       }
+//     });
+// }
+
+// mainPrompt();
